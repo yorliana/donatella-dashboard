e8ad16e271f2be6f5e085993fc76f4ccc751e602
yorliana <ing.yorlianam24@gmail.com>
2025-11-13 23:56:58 +0100

    Fix: configure Vercel output directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../dist',      // genera dist en la raíz del repo
+    outDir: '../dist',       // genera dist en la raíz
     chunkSizeWarningLimit: 1000
   }
 })
