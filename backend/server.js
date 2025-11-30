5e49339d63389d64c9eae062badd2ac054bb25b5
yorliana <ing.yorlianam24@gmail.com>
2025-11-30 23:39:00 +0100

    Actualizacion Backend

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,46 @@
 import express from "express";
-
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import donutRoutes from "./routes/donutRoutes.js";
 import ventaRoutes from "./routes/ventaRoutes.js";
-
 import pedidosRoutes from "./routes/pedidos.routes.js";
 
 import connectDB from "./config/db.js";
 
 dotenv.config();
-const express = require('express');
+
+// Inicializar express
 const app = express();
+
+// Middlewares
 app.use(express.json());
-//Middleware para leer body
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors({
-  origin: "http://localhost:5173", // Tu frontend
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type"]
 }));
 
-
-app.use(express.urlencoded({ extended: true }));
-
-// Rutas principales
+// Rutas
 app.use("/api/donuts", donutRoutes);
 app.use("/api/ventas", ventaRoutes);
 app.use("/api/pedidos", pedidosRoutes);
-app.get("/", (req, res) => {
+
+app.get("/api", (req, res) => {
   res.send("API Donas funcionando correctamente 🍩");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Servidor corriendo en puerto ${PORT}`));
-
-// Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI)
+// Conexión a Mongo
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("🍩 Conectado a MongoDB"))
   .catch(err => console.error("❌ Error en la conexión:", err));
 
-
-
+// Servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`🚀 Servidor corriendo en puerto ${PORT}`)
+);
