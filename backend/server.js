cea33e20bbf6bca7267e12e4ece40c73bc7d56e6
yorliana <ing.yorlianam24@gmail.com>
2025-12-01 00:29:38 +0100

    actualizacion package.js

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
   origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"]
+  methods: ["GET", "POST", "PUT", "DELETE,PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", Authorization],
+  credentials: true,
 }));
 
 // Rutas
